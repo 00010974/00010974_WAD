f95e0b1d0c65d98f124bd62127fd43a81417321c
brnshll <sabrina290103@gmail.com>
2023-04-01 20:00:34 +0500

    зги

---
+++ 00010974/Controllers/PublisherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _00010974.Controllers
+{
+    public class PublisherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
